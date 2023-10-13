7041a4a140d6cdc97a5f8c984ed3ff7531797ebf
IbrohimSayid <ibrahimoff.uz@gmail.com>
2023-10-13 21:41:52 +0500

    test

---
+++ app.js
@@ -0,0 +1,186 @@
+// <-------------------------------------------------------------------------------------->
+
+// Switch case - 1
+
+// let a = + prompt("1 dan 7 gacha son kiritng");
+
+// switch (a) {
+//     case 1:
+//         document.write("Dushanba");
+//         break;
+//     case 2:
+//         document.write("Seshanba");
+//         break;
+//     case 3:
+//         document.write("Chorshanba");
+//         break;
+//     case 4:
+//         document.write("Payshanba");
+//         break;
+//     case 5:
+//         document.write("Juma");
+//         break;
+//     case 6:
+//         document.write("Shanba");
+//         break;
+//     case 7:
+//         document.write("Shanba");
+//         break
+//     default:
+//         document.write("Notog'ri son kiritingiz iltimos 1 dan 7 gacha son kiriting")
+// }
+
+// <-------------------------------------------------------------------------------------->
+
+// Switch case - 2
+
+// let a = + prompt("1 dan 5 gacha bo'lgan son kiriting");
+
+// switch (a) {
+//     case 1:
+//         document.write("Yomon");
+//         break;
+//     case 2:
+//         document.write("Qoniqarsiz");
+//         break;
+//     case 3:
+//         document.write("Qoniqarli");
+//         break;
+//     case 4:
+//         document.write("Yaxshi");
+//         break;
+//     case 5:
+//         document.write("a'lo");
+//         break;
+//     default:
+//         document.write("Notog'ri son kiritildi iltimos etibor bering")
+// }
+
+// <-------------------------------------------------------------------------------------->
+
+// Switch case - 3
+
+// let a = + prompt("1 dan 12 gcha son kiritng");
+
+// switch (a) {
+//     case 1:
+//         document.write("Qish fasli 2chi oy");
+//         break;
+//     case 2:
+//         document.write("Qish fasli 3chi oy");
+//         break;
+//     case 3:
+//         document.write("Bahor fasli 1chi oy");
+//         break;
+//     case 4:
+//         document.write("Bahor fasli 2chi oy");
+//         break;
+//     case 5:
+//         document.write("Bahor fasli 3chi oy");
+//         break;
+//     case 6:
+//         document.write("Yoz fasli 4chi oy");
+//         break;
+//     case 7:
+//         document.write("Yoz fasli 2chi oy");
+//         break;
+//     case 8:
+//         document.write("Yoz fasli 3chi oy");
+//         break;
+//     case 9:
+//         document.write("Kuz fasli 1chi oy");
+//         break;
+//     case 10:
+//         document.write("Kuz fasli 2chi oy");
+//         break;
+//     case 11:
+//         document.write("Kuz fasli 3chi oy");
+//         break;
+//     case 12:
+//         document.write("Qish fasli 1chi oy");
+//     default:
+//         document,write("Notog'ri malumot iltimos etiborli bo'ling)");
+// }
+
+
+// <--------------------------------------------------------------------------->
+
+
+// Switch case - 4
+
+// let a = + prompt("Oy raqamini kiritng")
+
+// switch (a) {
+//     case 1:
+//         document.write("Birinchi oy Yanvar oyi va u 30 kundan ibrota");
+//         break;
+//     case 2:
+//         document.write("Ikkinchi oy Fevral oyi va 31 kundan iborat");
+//         break;
+//     case 3:
+//         document.write("Uchinchi oy Mart oyi va 30 kundan iborat");
+//         break;
+//     case 4:
+//         document.write("To'rtinchi oy Aprel oyi va 31 kundan iborat");
+//         break;
+//     case 5:
+//         document.write("Beshinchi oy May oyi va 30 kundan iborat");
+//         break;
+//     case 6:
+//         document.write("Oltinchi oy Iyun va 31 kundan iborat");
+//         break;
+//     case 7:
+//         document.write("Yettinchi oy Iyul va 30 kundan iborat");
+//         break;
+//     case 8:
+//         document.write("Sakizinchi oy Avgust va 31 kundan iborat");
+//         break;
+//     case 9:
+//         document.write("To'qqizinchi oy Sentabr va 30 kundan iborat");
+//         break;
+//     case 10:
+//         document.write("O'ninchi oy Oktabr va 31 kundan iborat");
+//         break;
+//     case 11:
+//         document.write("O'n birinchi oy va 30 kundan iborat");
+//         break;
+//     case 12:
+//         document.write("O'n ikkinchi oy va u 31 kundan iborat");
+//     default:
+//         document.write("Notog'ri malumot iltimos tekshirib kiritng");
+// }
+
+
+// <----------------------------------------------------------------------------->
+
+
+// Switch case - 5
+
+// let a = + prompt("birinchi son kiritng");
+// let b = + prompt("ikkinchi son kiritng");
+
+// let c = + prompt("biron bir son tanlan 1dan 4gacha")
+
+// switch (c) {
+//     case 1:
+//         document.write(a + b);
+//         break;
+//     case 2:
+//         document.write(a - b);
+//         break;
+//     case 3:
+//         document.write(a * b);
+//         break;
+//     case 4:
+//         document.write(a / b);
+//     default:
+//         document.write("Xatolik iltimos etior bering");
+// }
+
+
+// <-------------------------------------------------------------------------------------->
+
+
+// Switch case - 6
+
+let a = + prompt("")
